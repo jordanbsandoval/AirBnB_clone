84870ae184b75e87e876b1bc584f9f73fd67a675
jordanbsandoval <1608@holbertonschool.com>
2021-06-06 18:53:37 -0400

    flask_route-value-default

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
-# importando librerias
+"""importando librerias"""
 from flask import Flask
-
-# obteniendo objeto de la libreria flask
 app = Flask(__name__)
 
-# utilizando el modulo route
+
 @app.route('/', strict_slashes=False)
 def helloHtn():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def helloHbnb():
     return 'HBNB'
 
+
 @app.route('/c/<name>', strict_slashes=False)
 def cprofile(name):
-    return "C " + str(name)
+    return "C " + str(name).replace("_", " ")
 
-@app.route('/python/', defaults={'text':'is cool'}, strict_slashes=False)
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def aprofile(text):
-    return "Python " + str(text)
-    
+    return "Python " + str(text).replace("_", " ")
 
-# condicion 
+"""condicion"""
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
