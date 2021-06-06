84cc85ab3b35c9b76ea5c34151f47d8208c24fbf
jordanbsandoval <1608@holbertonschool.com>
2021-06-06 18:32:23 -0400

    pep8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""importando la libreria flask"""
 from flask import Flask
 app = Flask(__name__)
 
 
+"""modulo route"""
 @app.route('/', strict_slashes=False)
 def helloHbnb():
     return "Hello HBNB!"
 
+"""condicion"""
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
