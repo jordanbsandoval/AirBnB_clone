f6feaf9de5a5d50dc1066a74d673f676b7cd8505
jordanbsandoval <1608@holbertonschool.com>
2021-06-06 22:39:40 +0000

    flask_route-variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# importando librerias
+from flask import Flask
+
+# obteniendo objeto de la libreria flask
+app = Flask(__name__)
+
+# utilizando el modulo route
+@app.route('/', strict_slashes=False)
+def helloHtn():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def helloHbnb():
+    return 'HBNB'
+
+@app.route('/c/<name>', strict_slashes=False)
+def cprofile(name):
+    return "C " + str(name)
+    
+
+# condicion 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
