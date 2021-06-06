72c19634fa38e67b5113ce57ccd7cfb2b374e7a4
jordanbsandoval <1608@holbertonschool.com>
2021-06-06 18:37:46 -0400

    route-flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-# importando librerias
+"""importando librerias"""
 from flask import Flask
-
-# obteniendo objeto de la libreria flask
+"""obteniendo objeto de la libreria flask"""
 app = Flask(__name__)
 
-# utilizando el modulo route
+
 @app.route('/', strict_slashes=False)
 def helloHtn():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def helloHbnb():
     return 'HBNB'
 
-# condicion 
+"""condicion port"""
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
