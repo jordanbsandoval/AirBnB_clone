70d21f943cb7dd5c63f4d5ab88c05564a743da37
jordanbsandoval <1608@holbertonschool.com>
2021-06-06 19:14:13 -0400

    library_render-template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,42 +1,39 @@
 #!/usr/bin/python3
-# importando librerias
+"""importando librerias"""
 from flask import Flask, render_template
-
-# obteniendo objeto de la libreria flask
 app = Flask(__name__)
 
-# utilizando el modulo route
+
 @app.route('/', strict_slashes=False)
 def helloHtn():
     return 'Hello HBNB!'
 
-# Utilizando el modulo route
+
 @app.route('/hbnb', strict_slashes=False)
 def helloHbnb():
     return 'HBNB'
 
-#Utilizando el modulo route y escribiendo el valor de la variable pasada como argumento
+
 @app.route('/c/<name>', strict_slashes=False)
 def cprofile(name):
     return "C " + str(name)
 
-# Utilizando la funcion route y guardando el valor de una variable por defecto almacenada en una url
-@app.route('/python/', defaults={'text':'is cool'}, strict_slashes=False)
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def aprofile(text):
     return "Python " + str(text)
-    
-# Utilizando la funcion route y definiendo el valor de la variable
+
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def vprofile(n):
     return '%d is a number' % n
 
-# Utilizando el modulo render_template
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def nprofile(n):
     return render_template('index.html', username=n)
 
-# condicion 
+""" condicion """
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
