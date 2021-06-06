4a6e68a424eeb829241a021a10f3d1767ec9a807
jordanbsandoval <1608@holbertonschool.com>
2021-06-06 19:30:28 -0400

    hotfix

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -17,13 +17,13 @@ def helloHbnb():
 
 @app.route('/c/<name>', strict_slashes=False)
 def cprofile(name):
-    return "C " + str(name)
+    return "C " + str(name).replace("_", " ")
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def aprofile(text):
-    return "Python " + str(text)
+    return "Python " + str(text).replace("_", " ")
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
@@ -33,7 +33,7 @@ def vprofile(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def nprofile(n):
-    return render_template('index.html', username=n)
+    return render_template('5-number.html', username=n)
 
 """ condicion """
 if __name__ == '__main__':
