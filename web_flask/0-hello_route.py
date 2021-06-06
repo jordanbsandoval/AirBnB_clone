fa555144e0c5cb7e5c7f73d9f8406f9625bf56f0
jordanbsandoval <1608@holbertonschool.com>
2021-06-05 23:57:49 -0400

    framework-flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Write a script that starts a Flask web application:
+# Importando la libreria flask
+from flask import Flask
+
+# Guardando el modulo de la libreria en un variable objeto
+app = Flask(__name__)
+
+# Utilizando el modulo route para mostrar un mensaje cuando ingresen a la url
+@app.route('/' strict_slashes=False)
+def saludo():
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run('host=0.0.0.0', port=5000, debug=True)
